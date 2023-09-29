65529ee86f9349807633fe55163735a56c38f7b7
yesh-odoo <yesh@odoo.com>
2023-09-29 15:41:25 -0400

    [FIX] Syntax error

--- library/__manifest__.py
+++ library/__manifest__.py
@@ -1,11 +1,11 @@
 {
     'name': 'Odoo Library',
-    'summary': """Module to handle Books and Lending Information."""
+    'summary': """Module to handle Books and Lending Information.""",
     'description': """Module to handle:
         - Books
         - Lending
         - Students
-        """
+        """,
     'license': 'OPL-1',
     'author': 'yesh',
     'website': 'www.odoo.com',
