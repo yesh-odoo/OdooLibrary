72676c55fa023a58539f567fe522612df80efcfe
yesh-odoo <yesh@odoo.com>
2023-09-29 19:18:48 +0000

    [ADD] New Module

---
+++ library/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Library',
+    'summary': """Module to handle Books and Lending Information."""
+    'description': """Module to handle:
+        - Books
+        - Lending
+        - Students
+        """
+    'license': 'OPL-1',
+    'author': 'yesh',
+    'website': 'www.odoo.com',
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
